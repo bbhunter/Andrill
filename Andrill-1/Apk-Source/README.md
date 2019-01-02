99029ee85191977fc7d8daaa5072b841a26da39c
4lrz <alirezarezaie0101@gmail.com>
2019-01-02 15:48:11 +0330

    add android license for building apk

---
+++ Andrill-1/Apk-Source/README.md
@@ -0,0 +1,5 @@
+# build Apk 
+## linux
+```
+sh build-APK-here.sh server:port
+```
--- Andrill-1/Apk-Source/build-apk-here.sh
+++
@@ -1,5 +0,0 @@
-myarg=$1
-echo $myarg > ./app/src/main/res/raw/server.txt
-./gradlew assembleDebug
-rm -rf ./adnrill-1.apk
-cp ./app/build/outputs/apk/debug/app-debug.apk ./adnrill-1.apk
